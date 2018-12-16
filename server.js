b6dd64a6e7231698bc70b985267c87f13eeb3143
Disquietude <danny93ngo@gmail.com>
2018-12-16 05:13:27 -0500

    Added pug template for homepage

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 const app = express();
-
 import cors from 'cors';
-app.use(cors({optionsSuccessStatus: 200}));
+import { join } from 'path';
 
 require('dotenv').config();
+
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started on port " + process.env.PORT);
+});
