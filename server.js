4b4d358c2c489f5f7e3beb0961f6aec0fac355ae
Disquietude <danny93ngo@gmail.com>
2018-12-17 03:55:06 -0500

    Added API endpoint

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import cors from 'cors';
-import { join } from 'path';
+const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
 app.use(cors({optionsSuccessStatus: 200}));
 
 app.set('view engine', 'pug');
-app.set('views', join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.headers.host,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+})
 
 app.listen(process.env.PORT, () => {
   console.log("Server started on port " + process.env.PORT);
